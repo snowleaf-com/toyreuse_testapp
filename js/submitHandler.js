1ec996226a3ff1eba53b848082dcb4865bea485f
snowleaf-com <yy.dec5@gmail.com>
2024-10-26 22:23:41 +0900

    送信ボタンを押した時に編集モードか出品モードかでデータ取得を変える

--- js/submitHandler.js
+++ js/submitHandler.js
@@ -49,17 +49,35 @@ export function initSubmitButtonEvent(formId, buttonId) {
 
     // IndexedDBからデータを取得してフォームデータに追加
     const imagePaths = await getAllImages(); // IndexedDBから取得する関数
-        // 画像パスをそれぞれpic1, pic2, pic3としてFormDataに追加
-    imagePaths.forEach((image, index) => {
-      if (index === 0) {
-        formData.append('pic1', image.file);
-      } else if (index === 1) {
-        formData.append('pic2', image.file);
-      } else if (index === 2) {
-        formData.append('pic3', image.file);
+
+    // PHPから渡された画像データを追加
+    const productImages = [productsData.pic1, productsData.pic2, productsData.pic3];
+
+    // 空のカラム数をカウント
+    const emptyCount = productImages.filter(image => !image).length;
+
+    // 既存の画像をFormDataに追加
+    for (let i = 0; i < productImages.length; i++) {
+      if (productImages[i]) {
+        // 既存の画像があればそのまま追加
+        formData.append(`pic${i + 1}`, productImages[i]);
       }
-    });
-    
+    }
+
+    // IndexedDBからの画像を空いているカラムに追加
+    if (emptyCount > 0) {
+      // 空のカラムに応じてIndexedDBから画像を追加
+      let indexedImageIndex = 0; // IndexedDBの画像インデックス
+
+      for (let i = 0; i < productImages.length; i++) {
+        if (!productImages[i] && indexedImageIndex < imagePaths.length) {
+          // 空いているカラムにIndexedDBの画像を追加
+          formData.append(`pic${i + 1}`, imagePaths[indexedImageIndex].file);
+          indexedImageIndex++; // インデックスを進める
+        }
+      }
+    }
+
     // フォームデータをPHPに送信
     await submitFormWithImagePaths(formData);
   });
